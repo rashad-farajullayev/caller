0f1911767632b9d0ffd6218dfcb16a78598be059
Rashad Farajullayev <rashad.farajullayev@pashabank.az>
2018-05-21 14:08:19 +0400

    Add some more function to test the routing properly

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM java:8-jre-alpine
 WORKDIR /code/
-COPY target/demo-0.0.1-SNAPSHOT.jar target/application.properties ./
+COPY target/demo-0.0.1-SNAPSHOT.jar application.properties ./
 EXPOSE 8080
 CMD java -Dloader.path=. -jar demo-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/demo/CallerController.java
+++ src/main/java/com/example/demo/CallerController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+
 @RestController
 public class CallerController {
 
@@ -17,6 +22,40 @@ public class CallerController {
         RestTemplate tpl = new RestTemplate();
         String response = tpl.getForObject(endpointUrl, String.class);
 
-        return "the response is " + response;
+        return "the response is: " + response;
+    }
+
+    @GetMapping (value="/caller")
+    public String callService2 ()
+    {
+        try {
+            RestTemplate tpl = new RestTemplate();
+            String response = tpl.getForObject(endpointUrl, String.class);
+
+            return "By the \"/caller\" the response is: " + response;
+        }
+        catch (Exception ex){
+            return "Exception occurred: " + ex.getMessage();
+        }
+    }
+
+    @GetMapping (value="/caller/endpoint")
+    public String callService3 () throws MalformedURLException {
+
+        try {
+            RestTemplate tpl = new RestTemplate();
+            String url = endpointUrl;
+            if (!url.endsWith("/"))
+                url += "/";
+            url += "endpoint";
+            System.out.println(url);
+            String response = tpl.getForObject(url, String.class);
+
+            return "By the \"/caller\" the response is: " + response;
+        }
+        catch (Exception ex)
+        {
+            return "Exception occurred: " + ex.getMessage();
+        }
     }
 }
